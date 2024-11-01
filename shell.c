fa9c2ef36c043ef23beb3ae4f31aa40c25826951
liza <368051@edu.itmo.ru>
2024-11-01 14:48:47 +0300

    dop added

--- shell.c
+++ shell.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <windows.h>
 
-int main() {
-    char                command[1024];
-    STARTUPINFO         si;
-    PROCESS_INFORMATION pi;
-    LARGE_INTEGER       frequency, start, end;
-    double              elapsedTime;
+#define MAX_CMDS 10
+#define BUFFER_SIZE 1024
 
-    ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si);
-    ZeroMemory(&pi, sizeof(pi));
+int main() {
+    char          command[BUFFER_SIZE];
+    LARGE_INTEGER frequency, start, end;
+    double        elapsedTime;
 
     QueryPerformanceFrequency(&frequency);
 
     while (1) {
         printf("shell> ");
-        fgets(command, sizeof(command), stdin);
+        if (!fgets(command, sizeof(command), stdin)) {
+            break;
+        }
 
         command[strcspn(command, "\n")] = 0;
 
@@ -25,34 +25,124 @@ int main() {
             break;
         }
 
-        QueryPerformanceCounter(&start);
-
-        if (!CreateProcess(
-                NULL,     // lpApplicationName: путь к исполняемому файлу
-                command,  // lpCommandLine: командная строка для выполнения
-                NULL,     // lpProcessAttributes: NULL
-                NULL,     // lpThreadAttributes: NULL
-                FALSE,    // bInheritHandles: FALSE
-                0,        // dwCreationFlags: 0
-                NULL,     // lpEnvironment: NULL
-                NULL,     // lpCurrentDirectory: NULL
-                &si,      // lpStartupInfo: указатель на STARTUPINFO
-                &pi       // lpProcessInformation: указатель на PROCESS_INFORMATION
-                )) {
-            printf("Failed to start process (%d)\n", GetLastError());
+        if (strncmp(command, "cd ", 3) == 0) {
+            const char *path = command + 3;
+            if (SetCurrentDirectory(path)) {
+                char cwd[MAX_PATH];
+                GetCurrentDirectory(sizeof(cwd), cwd);
+                printf("Current directory: %s\n", cwd);
+            } else {
+                printf("Failed to change directory to %s\n", path);
+            }
             continue;
         }
 
-        WaitForSingleObject(pi.hProcess, INFINITE);
+        char *commands[MAX_CMDS];
+        int   num_commands = 0;
+
+        char *cmdline = _strdup(command);
+        if (!cmdline) {
+            fprintf(stderr, "Memory allocation error\n");
+            continue;
+        }
+
+        char *next_token = NULL;
+        char *token      = strtok_s(cmdline, "|", &next_token);
+        while (token != NULL && num_commands < MAX_CMDS) {
+            while (*token == ' ') token++;
+            char *end = token + strlen(token) - 1;
+            while (end > token && (*end == ' ' || *end == '\n')) {
+                *end = '\0';
+                end--;
+            }
+            commands[num_commands++] = token;
+            token                    = strtok_s(NULL, "|", &next_token);
+        }
+
+        HANDLE hPipeRead[MAX_CMDS], hPipeWrite[MAX_CMDS];
+        for (int i = 0; i < MAX_CMDS; i++) {
+            hPipeRead[i]  = NULL;
+            hPipeWrite[i] = NULL;
+        }
+
+        SECURITY_ATTRIBUTES saAttr;
+        saAttr.nLength              = sizeof(SECURITY_ATTRIBUTES);
+        saAttr.bInheritHandle       = TRUE;
+        saAttr.lpSecurityDescriptor = NULL;
+
+        for (int i = 0; i < num_commands - 1; i++) {
+            if (!CreatePipe(&hPipeRead[i], &hPipeWrite[i], &saAttr, 0)) {
+                fprintf(stderr, "Failed to create pipe\n");
+                goto cleanup;
+            }
+        }
+
+        PROCESS_INFORMATION pi[MAX_CMDS];
+        ZeroMemory(&pi, sizeof(pi));
+
+        QueryPerformanceCounter(&start);
+
+        for (int i = 0; i < num_commands; i++) {
+            STARTUPINFO si;
+            ZeroMemory(&si, sizeof(si));
+            si.cb = sizeof(si);
+            si.dwFlags |= STARTF_USESTDHANDLES;
+
+            if (i == 0) {
+                si.hStdInput  = GetStdHandle(STD_INPUT_HANDLE);
+                si.hStdOutput = (num_commands > 1) ? hPipeWrite[i] : GetStdHandle(STD_OUTPUT_HANDLE);
+            } else if (i == num_commands - 1) {
+                si.hStdInput  = hPipeRead[i - 1];
+                si.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+            } else {
+                si.hStdInput  = hPipeRead[i - 1];
+                si.hStdOutput = hPipeWrite[i];
+            }
+
+            si.hStdError = si.hStdOutput;
+
+            char cmdline_full[BUFFER_SIZE];
+            snprintf(cmdline_full, sizeof(cmdline_full), "cmd.exe /c \"%s\"", commands[i]);
+
+            if (!CreateProcess(
+                    NULL,
+                    cmdline_full,
+                    NULL,
+                    NULL,
+                    TRUE,
+                    0,
+                    NULL,
+                    NULL,
+                    &si,
+                    &pi[i])) {
+                fprintf(stderr, "Failed to start process (%d)\n", GetLastError());
+                goto cleanup;
+            }
+
+            if (i > 0) {
+                CloseHandle(hPipeRead[i - 1]);
+            }
+            if (hPipeWrite[i] != NULL) {
+                CloseHandle(hPipeWrite[i]);
+            }
+        }
+
+        for (int i = 0; i < num_commands; i++) {
+            WaitForSingleObject(pi[i].hProcess, INFINITE);
+            CloseHandle(pi[i].hProcess);
+            CloseHandle(pi[i].hThread);
+        }
 
         QueryPerformanceCounter(&end);
-
-        elapsedTime =
-            (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
+        elapsedTime = (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
         printf("Program executed in %.3f seconds\n", elapsedTime);
 
-        CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
+    cleanup:
+        for (int i = 0; i < MAX_CMDS; i++) {
+            if (hPipeRead[i]) CloseHandle(hPipeRead[i]);
+            if (hPipeWrite[i]) CloseHandle(hPipeWrite[i]);
+        }
+        free(cmdline);
     }
 
     return 0;
